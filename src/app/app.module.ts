8712acbe719a921cf69ec149f6bfefbb66b121a4
ludwig <murimiludwig@gmail.com>
2022-04-14 10:12:50 +0300

    this will add some files and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { DatePipe } from './date-pipe.pipe/date-pipe.pipe.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RepositoriesComponent,
+    SearchFormComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
