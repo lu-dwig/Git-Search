a1f7fc961bb96e643badcae532bd9b25319e3524
ludwig <murimiludwig@gmail.com>
2022-04-17 09:26:09 +0300

    this will add styling to the repo file

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
