eeed827b2131f555cfccc046e756729347e9460a
ludwig <murimiludwig@gmail.com>
2022-04-17 09:33:48 +0300

    this will modify the repo.ts file

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Userservicesevice } from '../userservices.services'
+import { Repos } from ',,/Repos'
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoriesComponent implements OnInit {
   
+  repo: Repos;
   constructor() { }
 
-  ngOnInit() {
+  repoSearch(searchName){
+    this.repoService.getReopos(searchName).then(
+      (results)=>{
+        this.repo =this.repoService.allRepos
+        console.log(this.repo);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
   }
 
+  ngOnInit() {
+    this.repoSearch('lu-dwig')
+  }
 }
